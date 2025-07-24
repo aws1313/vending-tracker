eeb93250aaec19e04becf3b1487fff0cbbce0a36
aws1313 <69417093+aws1313@users.noreply.github.com>
2025-07-24 10:51:32 +0200

    main django setup

---
+++ vt_web/start-dev.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+export DEBUG=True
+# Apply database migrations
+echo "Applying database migrations..."
+python manage.py migrate
+
+# Create a superuser if no users exist
+echo "Creating superuser if no users exist..."
+python manage.py createsuperuserifnone
+
+python manage.py collectstatic --noinput
+
+# Start the Django server
+echo "Starting Django server..."
+python manage.py runserver 0.0.0.0:8000
